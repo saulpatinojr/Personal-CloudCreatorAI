e3a43c3a57fa96db0fe15f66f25ae0a54ed84544
saulpatinojr <saulpatinojr@hotmail.com>
2025-09-24 13:48:46 -0500

    Working v0.1

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,10 +1,8 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
-import next from '@genkit-ai/next';
 
 export const ai = genkit({
   plugins: [
     googleAI(),
-    next(), // Add the Next.js plugin for proper integration.
   ],
 });
